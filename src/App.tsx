f69c1073ce0e1214c0417264d24a6fc023ae0d9a
Handayani Meiatara <91212078+Meiatara@users.noreply.github.com>
2023-08-24 09:08:11 +0700

    Membuat Todo

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import { FaPlus } from "react-icons/fa6";
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<
+    { id: number; text: string; completed: boolean }[]
+  >([]);
+  const [newTodoText, setNewTodoText] = useState("");
+
+  const addTodo = () => {
+    if (newTodoText.trim() === "") return;
+    const newTodo = { id: Date.now(), text: newTodoText, completed: false };
+    setTodos([...todos, newTodo]);
+    setNewTodoText("");
+  };
+
+  const toggleTodo = (id: number) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id: number) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  const editTodo = (id: number, newText: string) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, text: newText } : todo
+    );
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-4 bg-white rounded-md shadow min-w-md">
+        <h1 className="flex justify-center mt-6 mb-4 text-2xl font-semibold text-gray-950">
+          Todo List
+        </h1>
+        <div className="flex mb-4">
+          <input
+            type="text"
+            value={newTodoText}
+            onChange={(e) => setNewTodoText(e.target.value)}
+            placeholder="Add a new Todo..."
+            className="flex-grow px-3 py-2 text-gray-900 bg-gray-200 border rounded focus:border-blue-500 focus:outline-none"
+          />
+          <button
+            onClick={addTodo}
+            className="px-3 py-1 ml-2 text-white bg-blue-500 rounded hover:shadow-lg hover:bg-blue-800 focus:outline-none"
+          >
+            <FaPlus />
+          </button>
+        </div>
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            onToggle={toggleTodo}
+            onDelete={deleteTodo}
+            onEdit={editTodo}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Todo.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { FaPen, FaTrash, FaCheck, FaX } from "react-icons/fa6";
+
+interface TodoProps {
+  todo: { id: number; text: string; completed: boolean };
+  onToggle: (id: number) => void;
+  onDelete: (id: number) => void;
+  onEdit: (id: number, newText: string) => void;
+}
+
+const Todo: React.FC<TodoProps> = ({ todo, onToggle, onDelete, onEdit }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(todo.text);
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = () => {
+    onEdit(todo.id, editedText);
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setEditedText(todo.text);
+    setIsEditing(false);
+  };
+
+  return (
+    <div
+      className={`flex items-center mb-2 p-2 border-2 border-gray-200 rounded ${
+        todo.completed ? "line-through text-gray-500" : ""
+      }`}
+    >
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={() => onToggle(todo.id)}
+        className="mr-2"
+      />
+      {isEditing ? (
+        <div className="flex-grow">
+          <input
+            type="text"
+            value={editedText}
+            onChange={(e) => setEditedText(e.target.value)}
+            className="flex-grow px-3 py-2 text-gray-900 bg-gray-200 border rounded focus:border-blue-500 focus:outline-non"
+          />
+        </div>
+      ) : (
+        <div className="flex-grow">
+          <span className="text-gray-950">{todo.text}</span>
+        </div>
+      )}
+      <div>
+        {isEditing ? (
+          <>
+            <button
+              onClick={handleSave}
+              className="ml-1 text-green-600 bg-transparent border-gray-500 hover:border-green-600"
+            >
+              <FaCheck />
+            </button>
+            <button
+              onClick={handleCancel}
+              className="ml-1 text-red-600 bg-transparent border-gray-500 hover:border-red-600"
+            >
+              <FaX />
+            </button>
+          </>
+        ) : (
+          <>
+            <button
+              onClick={handleEdit}
+              className="ml-1 text-blue-600 bg-transparent border-gray-500"
+            >
+              <FaPen />
+            </button>
+            <button
+              onClick={() => onDelete(todo.id)}
+              className="ml-1 text-red-600 bg-transparent border-gray-500 hover:border-red-600"
+            >
+              <FaTrash />
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
